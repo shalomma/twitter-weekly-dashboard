24a0e92cdd6988b292cd9021b22b92e84625d365
Maayan Shalom <sha.maayan@gmail.com>
2025-01-02 15:24:36 +0200

    CLERK

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
-      <body className={inter.className}>{children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
+        <ClerkProvider>
+          <body className={inter.className}>{children}
+            <Analytics />
+            <SpeedInsights />
+          </body>
+        </ClerkProvider>
     </html>
   );
 }
