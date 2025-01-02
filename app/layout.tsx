842ee9bcab7509878b9a95492fad664ab840a254
Maayan Shalom <sha.maayan@gmail.com>
2025-01-02 12:04:47 +0200

    update layout with analytics and speedup apis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
